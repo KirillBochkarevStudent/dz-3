896ad25d848fe2787d2761cd93212908c8a91323
KirillBochkarevStudent <138363732+KirillBochkarevStudent@users.noreply.github.com>
2023-12-30 21:54:39 +0300

    Update StreamComporator.java

--- dz3/StreamComporator.java
+++ dz3/StreamComporator.java
@@ -5,6 +5,9 @@ public class StreamComporator implements Comparator<StudentGroup> {
     @Override
     public int compare(StudentGroup o1, StudentGroup o2) {
         int resultOfComparing = o1.getStudentGroupName().compareTo(o2.getStudentGroupName());
+        if(resultOfComparing == 0){
+        return resultOfComparing;
+        }
         return resultOfComparing;
     }
     
